a23e9f9b18da593a23a3a39d4666858617732c29
Nhat Nguyen <nhatndm1193@gmail.com>
2020-05-16 22:37:24 +0800

    Initial Commit

---
+++ covid19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid19
+//
+//  Created by Nhat Nguyen on 16/5/20.
+//  Copyright © 2020 Nhat Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
